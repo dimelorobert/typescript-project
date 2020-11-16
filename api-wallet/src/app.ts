2f9d73a2fe25272e73b0300e1d316827a445aba4
Robert Hernandez Arenas <52069820+dimelorobert@users.noreply.github.com>
2020-11-16 04:15:05 +0100

    Insatalado dotenv y configurado variables de entorno de la app y node

--- api-wallet/src/app.ts
+++ api-wallet/src/app.ts
@@ -1,5 +1,15 @@
-import express = require('express');
+// Variables de entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.APP_ENV = process.env.APP_ENV || 'development';
 
+import dotenv = require("dotenv");
+dotenv.config({
+    path: `${__dirname}/../config/${process.env.APP_ENV}.env`,
+});
+console.log('FROM APP_FOO:', process.env.APP_FOO);
+
+// Importamos Express con su respectiva app
+import express = require('express');
 const app = express.application = express();
 app.get('/', (request, response) => {
     response.send('The server is ---[ OFF ]---');
